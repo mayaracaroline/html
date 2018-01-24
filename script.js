605a63a194e43a86f33c30ab38c1be1da259adf8
Mayara Santos <mayara.santos@catho.com>
2018-01-24 08:45:59 -0200

    Insert function fetch - ajax

--- script.js
+++ script.js
@@ -1,40 +1,8 @@
-var luke = {
-  'name': 'Luke Skywalker',
-  'height': '172',
-  'mass': '77',
-  'hair_color': 'blond',
-  'skin_color': 'fair',
-  'eye_color': 'blue',
-  'birth_year': '19BBY',
-  'gender': 'male',
-  'homeworld': 'https://swapi.co/api/planets/1/',
-  'films': [
-    'https://swapi.co/api/films/2/',
-    'https://swapi.co/api/films/6/',
-    'https://swapi.co/api/films/3/',
-    'https://swapi.co/api/films/1/',
-    'https://swapi.co/api/films/7/'
-  ],
-  'species': [
-    'https://swapi.co/api/species/1/'
-  ],
-  'vehicles': [
-    'https://swapi.co/api/vehicles/14/',
-    'https://swapi.co/api/vehicles/30/'
-  ],
-  'starships': [
-    'https://swapi.co/api/starships/12/',
-    'https://swapi.co/api/starships/22/'
-  ],
-  'created': '2014-12-09T13:50:51.644000Z',
-  'edited': '2014-12-20T21:17:56.891000Z',
-  'url': 'https://swapi.co/api/people/1/'
-};
-
-
 // Cria elementos de uma tabela //
-var table = document.getElementById("characters-table");
-var tblBody = document.getElementById("characters-tbody");
+var table = document.getElementById("characters-table");    // Para receber a tabela "characters-table"
+var tblBody = document.getElementById("characters-tbody");  // Para receber o tbody
+
+// Lista de propriedades que serão utilizadas no for
 var propList = [
   "name",
   "height",
@@ -43,27 +11,33 @@ var propList = [
   "eye_color",
   "birth_year",
   "gender",
-
 ];
 
-for (i = 0; i < characters.results.length; i++) // row
-{ 
-  var character = characters.results[i];
-  var row = document.createElement("tr");
-  var tdProperty = [];
 
-  for (j = 0; j < propList.length ; j++)
-  {
-    var prop = propList[j];
-    tdProperty[j] = document.createElement("td");
-    tdProperty[j].innerHTML = character[prop];
-    row.appendChild(tdProperty[j]);
-  }
-
-  tblBody.appendChild(row);  
+// Função createRows()
+// Descrição: Função que cria linhas de uma tabela.
+// Parâmetros: character - json que será usado para preencher as linhas da tabela.
+function createRows(characters) {
+      // Laço  para criar linhas, colunas e inserir os respectivos valores
+    for (i = 0; i < characters.results.length; i++)          // row
+    { 
+      var character = characters.results[i];                 // Para receber a collection de personagens
+      var row = document.createElement("tr");                // Cria a linha da tabela 
+      var tdProperty = [];                                   // Para receber as colunas (proporcional a qtd de 
+//                                                              propriedades) 
+      for (j = 0; j < propList.length ; j++)
+      {
+        var prop = propList[j];                              // Recebe uma propriedade de propList referente ao índice
+        tdProperty[j] = document.createElement("td");        // Cria um novo <td> a cada iteração do for
+        tdProperty[j].innerHTML = character[prop];           // Insere o valor referente a propriedade no <td> criado
+//                                                              anteriormente
+        row.appendChild(tdProperty[j]);
+      }
+      tblBody.appendChild(row);  
+    }
 }
 
-//console.log(Object.keys(characters.results[i]));
-//console.log(character);
+fetch('https://swapi.co/api/people/').then(response => {response.json().then(createRows)});
+
 // Inserir bordas na tabela //
 table.setAttribute("border", "1");
