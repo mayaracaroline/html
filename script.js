f67f1b95e3234bb0c8e6f6d306d342c985b1a951
Mayara Santos <mayara.santos@catho.com>
2018-01-25 13:02:58 -0200

    Add function previousPage()

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ var propList = [
   "gender",
 ];
 var nextUrl = 'https://swapi.co/api/people/';
+var previousUrl = null;
+var contPages = [];
 
 // Função updateRows()
 // Descrição: Função que cria e atualiza linhas de uma tabela.
@@ -23,10 +25,12 @@ function updateRows(characters) {
       // Laço  para criar linhas, colunas e inserir os respectivos valores
     for (i = 0; i < characters.results.length; i++)          // row
     { 
+      console.log(i);
       var character = characters.results[i];                 // Para receber a collection de personagens
       var row = document.createElement("tr");                // Cria a linha da tabela 
-      var tdProperty = [];                                   // Para receber as colunas (proporcional a qtd de 
-//                                                              propriedades) 
+      var tdProperty = [];                                  // Para receber as colunas (proporcional a qtd de 
+//                                                              propriedades)
+
       for (j = 0; j < propList.length ; j++)
       {
         var prop = propList[j];                              // Recebe uma propriedade de propList referente ao índice
@@ -37,18 +41,41 @@ function updateRows(characters) {
       }
       tblBody.appendChild(row);  
     }
+
+    // Controlar quando os botões Previous e Next devem aparecer //
     nextUrl = characters.next;                                // Para receber a url da próxima página
+    previousUrl = characters.previous;                        // Para receber a url da página anterior
+
+    if (nextUrl === null )
+    {
+      document.getElementById("nextPage").style.display = "none";
+    }
+    else if (previousUrl === null)
+    {
+      document.getElementById("previousPage").style.display = "none";
+    }
+    else
+    {
+      document.getElementById("nextPage").style.display = "inline";
+      document.getElementById("previousPage").style.display = "inline"; 
+    }
 }
 
 //Função nextPage()
-//Função que busca dados em uma url externa e preenche a tabela e busca também 
+//Descrição: Função que busca dados em uma url externa e preenche a tabela e busca também 
 //a próxima página.
 
 function nextPage(){
   fetch(nextUrl).then(response => {response.json().then(updateRows)});
 }
 
+//Função previousPage()
+//Descrição: Função que retorna dados da página anterior e preenche a tabela.
+function previousPage(){
+  fetch(previousUrl).then(response => {response.json().then(updateRows)})
+}
+
 // Inserir bordas na tabela //
 table.setAttribute("border", "1");
 
-nextPage();
+nextPage(); // chamada da função para preencher pela primeira vez a tabela
