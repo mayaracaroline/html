498cd39d5325ff8e1d615bb4f001c92a52d18c22
Mayara Santos <mayara.santos@catho.com>
2018-01-31 10:40:06 -0200

    Criada a função goTopage()

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//------------------------------------------- Declarações --------------------------------------------------------------
 // Cria elementos de uma tabela //
 var table = document.getElementById("characters-table");    // Para receber a tabela "characters-table"
 var tblBody = document.getElementById("characters-tbody");  // Para receber o tbody
@@ -16,10 +17,9 @@ var nextUrl = 'https://swapi.co/api/people/';
 var previousUrl = null;
 var currentUrl = []; // Array que recebe a URL de todas as páginas
 var btn = []; // Array para a criação de botões
+var currentPage = 1;
 
-
-// -------------------------------------------------- Functions --------------------------------------------------------
-
+// -------------------------- Functions ---------------------------------------//
 // Função updateRows()
 // Descrição: Função que cria e atualiza linhas de uma tabela.
 // Parâmetros: character - json que será usado para preencher as linhas da tabela.
@@ -31,7 +31,6 @@ function updateRows(characters) {
     {
         currentUrl[i] = "https://swapi.co/api/people/?page=" + (i+1); 
     }
-
     // Laço  para criar linhas, colunas e inserir os respectivos valores
     for (let i = 0; i < characters.results.length; i++)        // row
     { 
@@ -54,24 +53,26 @@ function updateRows(characters) {
     nextUrl = characters.next;                                          // Para receber a url da próxima página
     previousUrl = characters.previous;                                  // Para receber a url da página anterior
 
-    if (nextUrl === null )                                              // Indica última página
+    if (currentPage === 9 )                                              // Indica última página
     {
+      console.log("currentPage " + currentPage);
       document.getElementById("nextPage").style.display = "none";       // Oculta o botão Next
+      document.getElementById("previousPage").style.display = "inline"; //Mostra o botão Previous
     }
-    else if (previousUrl === null)                                      // Indica primeira página
+    else if (currentPage === 1)                                         // Indica primeira página
     {
+      console.log("currentPage " + currentPage);
       document.getElementById("previousPage").style.display = "none";   // Oculta o botão Previous
+      document.getElementById("nextPage").style.display = "inline";     // Mostra o botão Next  
     }
     else                                                                // Caso contrário mostra ambos os botões
     {
       document.getElementById("nextPage").style.display = "inline"; 
       document.getElementById("previousPage").style.display = "inline"; 
     }
-    //body.appendChild(text);
   return currentUrl; //Retorna a URL para a função createBtn
 }
-  
-
+// --------------------------------------------------------------------//
 // Função: createBtn()
 // Descrição: Função que cria os botões para navegar entre as páginas
 // Parâmetros: URL da página que será direcionada ao clicar no botão
@@ -80,45 +81,63 @@ function createBtn(currentUrl){
   //Laço para criação de botões para navegar entre as páginas 
   for (let i = 0; i < currentUrl.length; i++)
   {
+   let pageNumber = i+1;
     btn[i] = document.createElement("button");                    //Criação de botões conforme a quantidade de páginas
-    btn[i].setAttribute("class", "btn");                             //Definir class para os botões
-    btn[i].innerHTML = i + 1;                                     //Enumerar botões
+    btn[i].setAttribute("class", "btn");                          //Definir class para os botões
+    btn[i].setAttribute("data-page-number", pageNumber)
+    btn[i].innerHTML = pageNumber;                                     //Enumerar botões
     body.appendChild(btn[i]);
-    btn[i].addEventListener("click", function(){ currentPage(currentUrl[i]) }); //Adiciona onClick ao botão
-    btn[i].addEventListener("click", btnAnimation);
+    btn[i].addEventListener("click", function(){ goToPage(pageNumber) }); //Adiciona onClick ao botão
   }       
 }
 
-function btnAnimation(){
+// --------------------------------------------------------------------//
 
-  var btnAll = document.querySelectorAll(".btn");
-  btnAll.forEach(function(element){
+//Função: btnAnimation()
+//Descrição: Função que altera a cor do botão para indicar em qual página está.
+function btnAnimation(pageNumber){
+  var btnAll = document.querySelectorAll(".btn");                // Seleciona todos os elementos com a class .btn
+    btnAll.forEach(function(element){                            // Itera pela lista de elementos removendo a class btnActive
     element.classList.remove("btnActive");
   });
-
-  this.classList.add("btnActive");
+  var currentPageButton = document.querySelectorAll('[data-page-number="' + pageNumber + '"]'); //Seleciona o elemento com atributo data-page com valor = a i+1
+    currentPageButton.forEach(function(element){                 // Itera pela lista adicionando a class btnActive ao botão selecionado
+    element.classList.add("class","btnActive");
+  });
 }
 
-
+// --------------------------------------------------------------------//
+//Função: createPage()
+//Descrição: Função que cria a tabela quando a página é carregada pela primeira vez
+//Obs.: Cria os botões através da função createBtn
+function createPage(){
+  fetch(nextUrl).then(response => {response.json().then(updateRows).then(createBtn)});
+}
 //Função nextPage()
 //Descrição: Função que busca dados em uma url externa e preenche a tabela e busca também 
 //a próxima página.
-function createPage(){
-    fetch(nextUrl).then(response => {response.json().then(updateRows).then(createBtn)});
-}
 function nextPage(){
-  fetch(nextUrl).then(response => {response.json().then(updateRows)});
-}
+  goToPage(currentPage+1);
 
+}
+// --------------------------------------------------------------------//
 //Função previousPage()
 //Descrição: Função que retorna dados da página anterior e preenche a tabela.
 function previousPage(){
-  fetch(previousUrl).then(response => {response.json().then(updateRows)})
+    goToPage(currentPage-1);
 }
-
-function currentPage(url) {
+// --------------------------------------------------------------------//
+//Função: goToPage()
+//Descrição: Função que recebe um número de página, concatena a URL padrão
+//e direciona para essa página.
+//Obs.: Utiliza a updateRows() para preencher a tabela e btnAnimation para alterar a cor dos botões. 
+function goToPage(pageNumber) {
+  var url = "https://swapi.co/api/people/?page=" + pageNumber;
+  currentPage = pageNumber;
   fetch(url).then(response => {response.json().then(updateRows)})
+  btnAnimation(pageNumber);
 }
+// --------------------------------------------------------------------//
 
 // Inserir bordas na tabela //
 table.setAttribute("border", "1");
